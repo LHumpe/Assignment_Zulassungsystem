c836a26813ac60307218ff0e30b1ed454ad13d7d
LHumpe <l.humpe@hotmail.de>
2020-11-14 16:14:37 +0100

    SA1-22 Login Funktionalität und Design implementiert, Zulassungsauschuss User kann über Admin erstellt werden mit Password und is_ausschuss attribut

--- accountspace/admin.py
+++ accountspace/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import User, Bewerber
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 
-
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None,{
+            'fields':('first_name','last_name','email', 'is_ausschuss', ),
+        }),
+    )
 # Register your models here.
-admin.site.register(User, BaseUserAdmin)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Bewerber)
