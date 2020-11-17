51e1c85e1ed515ba01cc98a98971202e21cca158
Mo <63042732+t00m-dev@users.noreply.github.com>
2020-11-17 23:08:56 +0100

    # set debug to FALSE

--- InfoZulassung/settings.py
+++ InfoZulassung/settings.py
@@ -28,7 +28,7 @@ else:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['swt-das-team.herokuapp.com']
 
