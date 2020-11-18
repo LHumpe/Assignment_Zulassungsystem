cfe09d2208942a101136ff5f03311fa9c7fd9cd1
LHumpe <l.humpe@hotmail.de>
2020-11-18 21:53:34 +0100

    SA1-44_feature SA1-65_feature
    - Anzahl der zugelassenen, pending und abgelehnten Bewerbungen werden an das Frontend gesendet
    -Speichern wenn mehr als 200 bewerber zugelassen wurden ist nicht mehr möglich

--- admissionspace/views/ausschuss.py
+++ admissionspace/views/ausschuss.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, UpdateView
 
@@ -9,11 +9,28 @@ class ApplicationListView(ListView):
     model = Bewerbung
     template_name = 'admissionspace/admission/application_list.html'
 
-
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['approved_admissions'] = len(Bewerbung.objects.filter(status='A'))
+        context['pending_admissions'] = len(Bewerbung.objects.filter(status='P'))
+        context['declined_admissions'] = len(Bewerbung.objects.filter(status='D'))
+        return context
 
 
 class AdmissionApplicationUpdateView(UpdateView):
     model = Bewerbung
     template_name = 'admissionspace/admission/application_admission.html'
-    fields=['status']
-    success_url = reverse_lazy('application_list')
+    fields = ['status']
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enrolment_capacity'] = 200 - len(Bewerbung.objects.filter(status='A'))
+        return context
+
+    def form_valid(self, form):
+        object = form.save(commit=False)
+        if len(Bewerbung.objects.filter(status='A')) >= 200:
+            return redirect('application_list')
+        else:
+            object.save()
+            return redirect('application_list')
