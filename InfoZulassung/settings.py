a764aa418bae243b2365ff79e6077dcd368e225d
Mo <63042732+t00m-dev@users.noreply.github.com>
2020-11-19 12:21:35 +0100

    #SA1-61 Form für Passwort zurücksetzen erstellt

--- InfoZulassung/settings.py
+++ InfoZulassung/settings.py
@@ -119,6 +119,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
